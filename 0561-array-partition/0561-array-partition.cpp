9c32cd0f44155032e5eb8e7ce5d7f01557139f90
Ayush Tripathi <ayushtripathi9821@gmail.com>
2026-06-23 11:00:04 +0530

    Time: 15 ms (31.84%), Space: 32.1 MB (55.85%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-
         sort(nums.begin(), nums.end());
         
         int total_sum = 0;
-        int n = nums.size();
-
-        int l = 0;
-        while (l < n) {
-            int r = l + 1; 
-
-            total_sum += min(nums[l], nums[r]);
-
-            l = r + 1; 
+ 
+        for (int i = 0; i < nums.size(); i += 2) {
+            total_sum += nums[i];
         }
         
         return total_sum;
     }
 };
-    
