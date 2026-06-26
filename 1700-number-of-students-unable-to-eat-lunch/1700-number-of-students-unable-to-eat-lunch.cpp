f27a30c9b3625438c70316f03e75d92e330b18d1
Ayush Tripathi <ayushtripathi9821@gmail.com>
2026-06-26 09:00:09 +0530

    Time: 0 ms (100%), Space: 11.8 MB (69.12%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int counts[2] = {0, 0};
+       
+        for (int student : students) {
+            counts[student]++;
+        }
+       
+        for (int sandwich : sandwiches) {
+           
+            if (counts[sandwich] == 0) {
+                break;
+            }
+            counts[sandwich]--;
+        }
+        
+        return counts[0] + counts[1];
+    }
+};
